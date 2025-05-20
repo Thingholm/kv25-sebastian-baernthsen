ee81ea7f8a2d34194be3c71fabdf2ee47b416aac
Thingholm <rasmusthingholm@gmail.com>
2025-05-20 12:43:12 +0200

    fix: adjust formatting in page schema definition

--- studio/schemaTypes/documents/page.ts
+++ studio/schemaTypes/documents/page.ts
@@ -1,6 +1,7 @@
 import { defineField, defineType } from "sanity";
 import { defaultFieldGroups } from "../config/fieldGroups";
 import { DocumentIcon } from '@sanity/icons';
+import pageSections from "../fields/pageSections";
 
 
 export default defineType({
@@ -11,20 +12,21 @@ export default defineType({
     groups: defaultFieldGroups,
     fields: [
         defineField({
-        name: 'name',
-        title: 'Name',
-        type: 'string',
-        group: 'content',
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+            group: 'content',
         }),
         defineField({
-        name: 'slug',
-        title: 'Slug',
-        type: 'slug',
-        options: {
-            source: 'name',
-            maxLength: 96,
-        },
-        group: 'content',
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'name',
+                maxLength: 96,
+            },
+            group: 'content',
         }),
+        pageSections,
     ]
 })
