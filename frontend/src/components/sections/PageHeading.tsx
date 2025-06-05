65342f757d9dcbe67969ea04313923ddd90614d4
Thingholm <rasmusthingholm@gmail.com>
2025-06-05 14:52:12 +0200

    refactor: remove image rendering from PageHeading component

--- frontend/src/components/sections/PageHeading.tsx
+++ frontend/src/components/sections/PageHeading.tsx
@@ -9,17 +9,7 @@ type Props = {
 export default async function PageHeading({ section }: Props) {
     return (
         <section>
-            <div className="relative w-full h-52 md:h-64">
-                {section.image?.asset?._ref &&
-                    <Image
-                        src={urlFor(section.image?.asset?._ref).url()}
-                        alt={section.image.alt ?? ""}      
-                        fill     
-                        className="object-cover"     
-                    />
-                }
-            </div>
-            <h2 className="text-4xl font-bold text-center text-orange-500 py-4">{section.heading}</h2>
+            <h2 className="text-4xl sm:text-5xl font-bold text-center text-orange-500 py-8">{section.heading}</h2>
         </section>
     )
 }
