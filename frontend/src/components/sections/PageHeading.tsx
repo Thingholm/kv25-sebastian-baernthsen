577a5a08388558e20ffad0093adbd7becdf2cec6
Thingholm <rasmusthingholm@gmail.com>
2025-06-05 15:06:39 +0200

    style: adjust padding in PageHeading component

--- frontend/src/components/sections/PageHeading.tsx
+++ frontend/src/components/sections/PageHeading.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default async function PageHeading({ section }: Props) {
     return (
         <section>
-            <h2 className="text-4xl sm:text-5xl font-bold text-center text-orange-500 py-8">{section.heading}</h2>
+            <h2 className="text-4xl sm:text-5xl font-bold text-center text-orange-500 pt-8 pb-4">{section.heading}</h2>
         </section>
     )
 }
