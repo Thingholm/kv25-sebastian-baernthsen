e0ba61414530ab00a8d82aade07dca26e7c745c4
Thingholm <rasmusthingholm@gmail.com>
2025-10-29 20:00:17 +0100

    Revalidate endpoint

---
+++ frontend/src/app/api/revalidate/route.ts
@@ -0,0 +1,27 @@
+import { revalidatePath } from "next/cache";
+import type { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const secret = req.headers.get("x-secret");
+    if (secret !== process.env.REVALIDATE_SECRET) {
+      return new Response("Invalid secret", { status: 401 });
+    }
+
+    const payload = await req.json();
+    if (!payload) return new Response("No payload", { status: 400 });
+
+    if (payload.slug) {
+      revalidatePath(`/${payload.slug}`);
+    }
+    if (payload.slugs?.length) {
+      payload.slugs.forEach((s: string) => revalidatePath(`/${s}`));
+    }
+
+    if (payload.revalidateHome) revalidatePath("/");
+
+    return new Response(JSON.stringify({ revalidated: true }));
+  } catch (err) {
+    return new Response("Error revalidating", { status: 500 });
+  }
+}
